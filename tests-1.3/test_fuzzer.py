39c74441fd97befbe0e5573e89cae4f40259d584
sebastien <sebastien.coorevits@telecomnancy.eu>
2016-07-20 14:10:47 +0200

    random lgt

--- tests-1.3/test_fuzzer.py
+++ tests-1.3/test_fuzzer.py
@@ -26,7 +26,7 @@ class Echo(base_tests.SimpleProtocol):
     """
 
     def runTest(self):
-        request = ofp.message.echo_request()
+        request = ofp.message.echo_request(length=random.randint(5,20))
         response, pkt = self.controller.transact(request)
         while True:
             request = ofp.fuzzer.echo_request()
