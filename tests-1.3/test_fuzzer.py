b1633d594ebb09bd56fb325ed7f9dae55335038d
sebastien <sebastien.coorevits@telecomnancy.eu>
2016-08-26 17:10:42 +0200

    example

--- tests-1.3/test_fuzzer.py
+++ tests-1.3/test_fuzzer.py
@@ -11,10 +11,24 @@ from oftest.parser import *
 
 
 @group('smoke')
-class Echo(base_tests.SimpleProtocol):
+class Fuzz(base_tests.SimpleProtocol):
     """
-    Test echo response with no data
-    """
+    add(self,nameOfMessage,iteration,xid,length,version,type,bversion,btype,blength,bxid,data)
 
+    nameOfMessage : Only mandatory argument, it can either be "flow\_add", "echo" "flow\_del" (which delete all flows regardless of the argument) or "barrier".
+    iteration : number of times the message should be sent (default = 1)
+    xid : if you want to modify the xid of the message (int)
+    length : if you want to modify the length (int)
+    version : if you want to modify the version of Openflow, (int, default = 4 for 1.3)
+    type : this field is used to tell the switch what the message is, default : 14 for flow\_add, 2 for echo, and 20 for barrier.
+    bversion, btype, blength, bxid specified on how many bytes the field is set. It can be "!B", "!H" or "!L"
+    data : this field is relevant for echo messages which can contains data, default = None.
+
+    example
+    add(self,"echo",xid=42,data = "this is a test")
+    add(self,"flow\_add", iteration = 100000, bversion="!H")
+
+    """
     def runTest(self):
-        add(self,"echo", 1)
+        add(self,)
+
